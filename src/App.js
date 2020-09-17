c670ab2c03554edc19d9b69b2524f016951e952d
Adilson Mandlate <adilsonmandlate@gmail.com>
2020-09-17 19:20:02 +0200

    chore: remove unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello. My name is Adilson Mandlate.</p>
+        <p>Hello. My name is Adilson Mandlate!</p>
       </header>
     </div>
   );
