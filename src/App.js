f5690fa942ca018544714c6f7ba2cf4363d753ac
Adilson Mandlate <adilsonmandlate@gmail.com>
2020-09-18 13:17:21 +0200

    feat: navigate through different routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello. My name is Adilson Mandlate!</p>
-      </header>
+    <div className="main">
+      <Switch>
+        <Route path="/" render={() => <div>Render main page here!</div>} />
+        <Route
+          path="/todos/new"
+          render={() => <div>Render todos form here!</div>}
+        />
+        <Route
+          path="/categories/new"
+          render={() => <div>Render categories form here!</div>}
+        />
+      </Switch>
     </div>
   );
 }
