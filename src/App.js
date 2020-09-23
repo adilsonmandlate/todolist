e3baa3b6f8265f95f532267acc25c9c4fe9168cc
Adilson Mandlate <adilsonmandlate@gmail.com>
2020-09-23 22:16:52 +0200

    feat: render main page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import { Home } from "./pages";
 
 function App() {
   return (
     <div className="main">
       <Switch>
-        <Route path="/" render={() => <div>Render main page here!</div>} />
+        <Route path="/" component={Home} />
         <Route
           path="/todos/new"
           render={() => <div>Render todos form here!</div>}
@@ -14,6 +15,7 @@ function App() {
           path="/categories/new"
           render={() => <div>Render categories form here!</div>}
         />
+        <Route render={() => <div>Not found.</div>} />
       </Switch>
     </div>
   );
