83b4eed20bebd95e744a905dc891683031445429
Adilson Mandlate <adilsonmandlate@gmail.com>
2020-09-24 12:33:33 +0200

    feat: use app provider

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import { Home } from "./pages";
+import { StateProvider } from "./provider";
 
 function App() {
   return (
-    <div className="main">
-      <Switch>
-        <Route path="/" component={Home} />
-        <Route
-          path="/todos/new"
-          render={() => <div>Render todos form here!</div>}
-        />
-        <Route
-          path="/categories/new"
-          render={() => <div>Render categories form here!</div>}
-        />
-        <Route render={() => <div>Not found.</div>} />
-      </Switch>
-    </div>
+    <StateProvider>
+      <div className="main">
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route
+            path="/todos/new"
+            render={() => <div>Render todos form here!</div>}
+          />
+          <Route
+            path="/categories/new"
+            render={() => <div>Render categories form here!</div>}
+          />
+          <Route render={() => <div>Not found.</div>} />
+        </Switch>
+      </div>
+    </StateProvider>
   );
 }
 
