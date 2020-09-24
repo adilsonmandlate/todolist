d4833772d31155910190b807f7de4e095619f045
Adilson Mandlate <adilsonmandlate@gmail.com>
2020-09-24 17:17:59 +0200

    feat: list todos

---
+++ src/pages/Home/components/Todo.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Check, Delete, Edit } from "react-feather";
+import styles from "../styles/Todo.module.css";
+
+const Todo = ({ todo, late = false }) => {
+  const completeTodo = () => {
+    alert("Completed :)");
+  };
+
+  const removeTodo = () => {
+    alert("removing todo...");
+  };
+
+  return (
+    <li className={styles.container}>
+      <span onClick={() => completeTodo()} className={styles.status}>
+        <Check size={12} />
+      </span>
+      <div>
+        <p className={styles.name}>{todo.name}</p>
+        {late && <span className={styles.date}>{todo.date}</span>}
+      </div>
+      <div className={styles.actions}>
+        <span className={styles.editAction}>
+          <Edit size={18} />
+        </span>
+        <span className={styles.deleteAction} onClick={() => removeTodo()}>
+          <Delete size={18} />
+        </span>
+      </div>
+    </li>
+  );
+};
+
+export default Todo;
