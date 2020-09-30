226f9f31ed36a6606680a6fd04353a0f08d82344
Adilson Mandlate <adilsonmandlate@gmail.com>
2020-09-30 14:34:54 +0200

    feat: display input error when available

--- src/components/form/components/Input.js
+++ src/components/form/components/Input.js
@@ -5,8 +5,12 @@ const Input = ({ label, error, register, ...rest }) => {
   return (
     <div className={styles.container}>
       <label className={styles.label}>{label}</label>
-      <input className={styles.input} ref={register} {...rest} />
-      {error && <span className={styles.error}>{error}</span>}
+      <input
+        className={`${styles.input} ${error && styles.inputError}`}
+        ref={register}
+        {...rest}
+      />
+      {error && <span className={styles.error}>{error.message}</span>}
     </div>
   );
 };
