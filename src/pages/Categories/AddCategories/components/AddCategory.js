80771cbcab0b9f8c1beb08ec0e9fc484d300c66d
Adilson Mandlate <adilsonmandlate@gmail.com>
2020-11-19 08:57:22 +0200

    chore: create categories initial files

---
+++ src/pages/Categories/AddCategories/components/AddCategory.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddCategory = () => {
+  return <p>New category comes here!</p>;
+};
+
+export default AddCategory;
---
+++ src/pages/Categories/ListCategories/components/ListCategories.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ListCategories = () => {
+  return <p>List of categories comes here!</p>;
+};
+
+export default ListCategories;
