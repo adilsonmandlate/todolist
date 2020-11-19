4316e54f70298c2a8126bca8224c9904a9efd94e
Adilson Mandlate <adilsonmandlate@gmail.com>
2020-11-19 08:56:48 +0200

    feat: set a max size windom

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require("electron");
+const { app, dialog, Menu, BrowserWindow } = require("electron");
 const isDev = require("electron-is-dev");
 const path = require("path");
 
@@ -17,10 +17,50 @@ function createWindow() {
 
   mainWindow.loadURL(startURL);
 
+  mainWindow.setMinimumSize(750, 550);
+  mainWindow.setMaximumSize(900, 700);
+
   mainWindow.once("ready-to-show", () => mainWindow.show());
   mainWindow.on("closed", () => {
     mainWindow = null;
   });
 }
 
-app.on("ready", createWindow);
+function setupMenu() {
+  const menuTemplate = [
+    {
+      label: "todolist",
+      submenu: [
+        {
+          label: "About",
+          click: () => {
+            dialog.showMessageBox(mainWindow, {
+              type: "info",
+              title: "About",
+              message: "todolist is built by @adilsonmandlate",
+            });
+          },
+        },
+        {
+          type: "separator",
+        },
+
+        {
+          label: "Quit",
+          accelerator: "CommandOrControl+Q",
+          click: () => {
+            app.quit();
+          },
+        },
+      ],
+    },
+  ];
+
+  const menu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(menu);
+}
+
+app.on("ready", () => {
+  createWindow();
+  setupMenu();
+});
