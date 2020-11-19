bbfcede2e33d181fb3b227fb2719a35f01e2282e
Adilson Mandlate <adilsonmandlate@gmail.com>
2020-11-19 08:47:53 +0200

    feat: add an option to complete todos

--- src/provider/constants.js
+++ src/provider/constants.js
@@ -1,6 +1,7 @@
 export const ADD_TODO = "ADD_TODO";
 export const EDIT_TODO = "EDIT_TODO";
 export const DELETE_TODO = "DELETE_TODO";
+export const COMPLETE_TODO = "COMPLETE_TODO";
 
 export const ADD_LABEL = "ADD_LABEL";
 export const EDIT_LABEL = "EDIT_LABEL";
