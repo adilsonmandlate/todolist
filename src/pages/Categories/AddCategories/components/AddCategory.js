b42a7ebda63029cfa15db40949b687f1ddb267c0
Adilson Mandlate <adilsonmandlate@gmail.com>
2021-04-26 11:17:35 +0200

    chore: remove categories list and options

--- src/pages/Categories/AddCategories/components/AddCategory.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AddCategory = () => {
-  return <p>New category comes here!</p>;
-};
-
-export default AddCategory;
--- src/pages/Categories/ListCategories/components/ListCategories.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ListCategories = () => {
-  return <p>List of categories comes here!</p>;
-};
-
-export default ListCategories;
